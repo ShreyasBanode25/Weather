382defbe3126502d75fe5c067392150e139ae995
ShreyasBanode25 <banodeshreyas@gmail.com>
2024-08-12 12:11:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
+import React, { useState } from 'react';
+import Navbar from './Components/Navbar';
+import Cards from './Components/Cards';
+import Overview from './Components/Overview'; 
+import Search from './Components/Search';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  const [weatherData, setWeatherData] = useState(null);
+
+  const handleSearch = (data) => {
+    setWeatherData(data);
+  };
+
+  return (
+    <div className="App">
+      <Navbar onSearch={handleSearch} />
+      {weatherData && <Cards data={weatherData} />}
+      <Search />
+      <Overview /> {/* This will render the carousel */}
+    </div>
+  );
+}
+
+export default App;
 
